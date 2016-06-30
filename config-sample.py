6b5f7638148eb250294b1cad4a0443aaea355c69
Nico Rode <nico.rode.96@gmail.com>
2016-06-30 17:54:14 +0200

    - Adds bot functionality using the praw library - Adds requirements.txt, license file and gitignore file

---
+++ config-sample.py
@@ -0,0 +1,11 @@
+# Username of the bot account.
+username = ""
+# Password for the bot account.
+password = ""
+# List of subreddits to be watched by the bot.
+# The bot only comments inside these subreddits.
+subreddit_list = [""]
+# User agent for the bot, sent with each request to the reddit servers.
+# Should contain a way to be contacted in case the bot does something stupid.
+# Can not contain the word "bot".
+user_agent = "Sovereign Citizen Commenter 1.0 by /u/Calamity701"
---
+++ sovereign_citizen_bot/reddit_bot.py
@@ -0,0 +1,79 @@
+import os
+import traceback
+from time import sleep
+
+import praw
+
+from config import user_agent, username, password, subreddit_list, delay_between_polls
+from sovereign_citizen_bot.quote_generator import get_quote
+
+
+def check_config():
+    if not os.path.isfile("../config.py"):
+        print("You must create a config file")
+        exit(1)
+
+
+def login():
+    print("Logging in as /u/" + username + " with user agent " + user_agent)
+    r = praw.Reddit(user_agent=user_agent)
+    r.login(username=username, password=password)
+    return r
+
+
+def main():
+    already_replied = set()
+    check_config()
+    r = login()
+    while True:
+        try:
+            print("Looking for new mail")
+            for comment in r.get_unread():
+                print("Id: " + comment.id + "\nSubreddit: " + str(comment.subreddit) + "\nBody: " + comment.body)
+
+                if "/u/" + username not in comment.body:
+                    comment.mark_as_read()
+                    print("Comment is not a username mention")
+                    continue
+
+                if str(comment.subreddit) not in subreddit_list:
+                    comment.mark_as_read()
+                    print("Comment is not in subreddit list")
+                    continue
+
+                for reply in comment.replies:
+                    if reply.author is username:
+                        already_replied.add(reply.id)
+                        print("Already replied: " + reply)
+                        break
+
+                if comment.id not in already_replied:
+                    quote = get_quote()
+                    print("Commenting with quote '" + quote + "'")
+                    comment.reply(quote)
+                    comment.mark_as_read()
+            print("Sleeping...")
+            sleep(delay_between_polls)
+
+        # Stop if someone manually interrupts
+        except KeyboardInterrupt:
+            print("Keyboard interrupt received. Shutting down")
+            exit(0)
+
+        # Praw handles the API Rate limits for us, we just have to sleep and try again
+        except praw.errors.RateLimitExceeded:
+            print("Rate limit exceeded")
+            sleep(delay_between_polls)
+            continue
+
+        # Catch all exceptions so in case of a simple error the next loop will be successful
+        except Exception as e:
+            print(traceback.format_exc())
+            print("Error: ", e)
+            print("Waiting " + delay_between_polls + " seconds before trying again")
+            sleep(delay_between_polls)
+            continue
+
+
+if __name__ == "__main__":
+    main()
