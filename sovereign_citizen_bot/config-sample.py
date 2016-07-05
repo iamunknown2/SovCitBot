22d25a4f8c314a6e37b5376380f84583f1f210ce
iamunknown2 <iamunknown2@users.noreply.github.com>
2016-07-05 14:28:16 +0800

    Updated config-sample, changed .gitignore

--- sovereign_citizen_bot/config-sample.py
+++ sovereign_citizen_bot/config-sample.py
@@ -9,3 +9,5 @@ subreddit_list = [""]
 # Should contain a way to be contacted in case the bot does something stupid.
 # Can not contain the word "bot".
 user_agent = "Sovereign Citizen Commenter 1.0 by /u/Calamity701"
+# Seconds per refresh
+delay_between_polls = 15
